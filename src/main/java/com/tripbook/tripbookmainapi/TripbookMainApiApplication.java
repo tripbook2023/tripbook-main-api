909308d10f8616cd258d0c37c7b574ddc70dd61e
kyumin <kyumin.choi@nhnfashiongo.com>
2023-04-05 21:51:34 +0900

    first commit

---
+++ src/main/java/com/tripbook/tripbookmainapi/TripbookMainApiApplication.java
@@ -0,0 +1,13 @@
+package com.tripbook.tripbookmainapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripbookMainApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripbookMainApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tripbook/tripbookmainapi/TripbookMainApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripbook.tripbookmainapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripbookMainApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
