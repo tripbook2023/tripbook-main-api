4083b895cc62906e3984bf8a21c9f115628e57bf
Depther <victory2373@gmail.com>
2023-04-09 00:46:01 +0900

    chore: chage test api response format

--- src/main/java/com/tripbook/main/temp/TestController.java
+++ src/main/java/com/tripbook/main/temp/TestController.java
@@ -1,13 +1,20 @@
 package com.tripbook.main.temp;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class TestController {
 
+    private final TestService testService;
+
     @GetMapping("/test")
-    public String test() {
-        return "success";
+    public ResponseEntity test() {
+        System.out.println(testService.test());
+        return new ResponseEntity(HttpStatusCode.valueOf(200));
     }
 }
---
+++ src/main/java/com/tripbook/main/temp/TestService.java
@@ -0,0 +1,12 @@
+package com.tripbook.main.temp;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+    public boolean test() {
+        return true;
+    }
+
+}
