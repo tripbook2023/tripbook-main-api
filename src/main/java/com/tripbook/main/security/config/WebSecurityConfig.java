278d6db1192f722ca5397b87ad64a54dee3713b1
bondyuu <87316155+bondyuu@users.noreply.github.com>
2023-06-28 14:02:41 +0900

    Feat: 로컬환경에 H2 DB 설정

--- src/main/java/com/tripbook/main/security/config/WebSecurityConfig.java
+++ src/main/java/com/tripbook/main/security/config/WebSecurityConfig.java
@@ -42,6 +42,8 @@ public class WebSecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.headers().frameOptions().disable();
+
 		//PlatForm 토큰 인증필터
 		http
 			.authorizeHttpRequests()
@@ -50,6 +52,7 @@ public class WebSecurityConfig {
 			.addFilterBefore(oAuth2AccessTokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 		http
 			.authorizeHttpRequests()
+			.requestMatchers("/h2-console/**").permitAll()
 			.requestMatchers(HttpMethod.POST, "/member/signup")
 			.permitAll()
 			.requestMatchers(HttpMethod.POST, "/member/**")
