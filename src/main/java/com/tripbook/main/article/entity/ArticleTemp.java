7ec89c4fa23ddb4ff0a38c0c13ad27b76c5828e7
bondyuu <87316155+bondyuu@users.noreply.github.com>
2023-06-29 13:38:24 +0900

    Feat: Article 임시저장 엔티티 추가

---
+++ src/main/java/com/tripbook/main/article/entity/ArticleTemp.java
@@ -0,0 +1,41 @@
+package com.tripbook.main.article.entity;
+
+import com.tripbook.main.global.common.BasicEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "TB_ARTICLE_TEMP")
+@Getter
+@DynamicInsert
+@DynamicUpdate
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ArticleTemp extends BasicEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "articleTemp")
+    private List<ArticleTempImage> imageList = new ArrayList<>();
+
+    @Builder
+    public ArticleTemp(String title, String content, List<ArticleTempImage> imageList) {
+        this.title = title;
+        this.content = content;
+        this.imageList = imageList;
+    }
+}
---
+++ src/main/java/com/tripbook/main/article/repository/ArticleTempImageRepository.java
@@ -0,0 +1,7 @@
+package com.tripbook.main.article.repository;
+
+import com.tripbook.main.article.entity.ArticleTempImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleTempImageRepository extends JpaRepository<ArticleTempImage, Long> {
+}
