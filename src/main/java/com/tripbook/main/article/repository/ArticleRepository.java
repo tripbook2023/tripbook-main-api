91a95f7678fb96cd8f6e36991ab9f27406a18576
bondyuu <87316155+bondyuu@users.noreply.github.com>
2023-08-07 13:12:56 +0900

    fix: 활성상태의 값만 불러오도록 수정

--- src/main/java/com/tripbook/main/article/repository/ArticleRepository.java
+++ src/main/java/com/tripbook/main/article/repository/ArticleRepository.java
@@ -1,10 +1,12 @@
 package com.tripbook.main.article.repository;
 
 import com.tripbook.main.article.entity.Article;
+import com.tripbook.main.article.enums.ArticleStatus;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-    Slice<Article> findAllByTitleContainsAndContentContains(String title, String content, Pageable pageable);
+    Slice<Article> findAllByStatus(ArticleStatus status, Pageable pageable);
+    Slice<Article> findAllByTitleContainingAndContentContainingAndStatus(String title, String content, ArticleStatus status, Pageable pageable);
 }
