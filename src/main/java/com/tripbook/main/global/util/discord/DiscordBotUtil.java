92309a6ad378ea784dcf454e6809783ae1b5ac28
bondyuu <87316155+bondyuu@users.noreply.github.com>
2023-09-19 13:07:17 +0900

    fix: 디스코드 채널명 변경으로 인한 수정

--- src/main/java/com/tripbook/main/global/util/discord/DiscordBotUtil.java
+++ src/main/java/com/tripbook/main/global/util/discord/DiscordBotUtil.java
@@ -28,7 +28,7 @@ public class DiscordBotUtil {
         System.out.println(jda.getGuilds());
         Guild guild = jda.getGuilds().get(0);
         List<GuildChannel> channels = guild.getChannels();
-        TextChannel tc = (TextChannel) channels.stream().filter(c -> c.getName().equals("서버에러-발생이슈")).toList().get(0);
+        TextChannel tc = (TextChannel) channels.stream().filter(c -> c.getName().contains("서버에러")).toList().get(0);
 
         tc.sendMessage(object).queue();
     }
