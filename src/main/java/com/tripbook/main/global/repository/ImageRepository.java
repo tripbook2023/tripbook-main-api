72d2a72342bf004fafe1b1a54b4a050636d53d97
LeeSeokWoon <leeseokwoon@naver.com>
2023-12-01 22:26:02 +0900

    fix:이미지업로드_이슈수정
    updateByRefIdReset Return Type 변경
    updateController return 정보변경
    ResponseImage.ImageInfo 스키마설명 수정
    ResponseImage.ImageInfo PK값까지 전달

--- src/main/java/com/tripbook/main/global/repository/ImageRepository.java
+++ src/main/java/com/tripbook/main/global/repository/ImageRepository.java
@@ -7,14 +7,15 @@ import com.tripbook.main.member.entity.Member;
 import com.tripbook.main.token.enums.DeviceValue;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
 	List<Image> findByRefId(long refId);
 
-
+	@Modifying
 	@Query("update Image i set i.refId=null where i.refId=:refId")
-	 long updateByRefIdReset(@Param(value = "refId") long refId);
+	 void updateByRefIdReset(@Param(value = "refId") long refId);
 
 }
