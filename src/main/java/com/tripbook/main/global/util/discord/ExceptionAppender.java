ce98aee8f27007ab7828e606d3e79dfc031b80ed
lso5507 <49707719+lso5507@users.noreply.github.com>
2024-02-03 15:59:44 +0900

    feat:디스코드봇_운영환경에서만동작

--- src/main/java/com/tripbook/main/global/util/discord/ExceptionAppender.java
+++ src/main/java/com/tripbook/main/global/util/discord/ExceptionAppender.java
@@ -1,6 +1,5 @@
 package com.tripbook.main.global.util.discord;
 
-import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -8,59 +7,61 @@ import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 @Aspect
 @Component
 @AutoConfigurationPackage
-@Profile({"prod", "dev"})
+@Profile({"prod"})
 public class ExceptionAppender {
 
-    private static final String DISCORD_ALARM_FORMAT = "[DiscordAlarm] %s";
+	private static final String DISCORD_ALARM_FORMAT = "[DiscordAlarm] %s";
 
-    private final RequestStorage requestStorage;
-    private final DiscordMessageGenerator discordMessageGenerator;
-    private final DiscordBotUtil discordBotUtil;
+	private final RequestStorage requestStorage;
+	private final DiscordMessageGenerator discordMessageGenerator;
+	private final DiscordBotUtil discordBotUtil;
 
-    public ExceptionAppender(RequestStorage requestStorage,
-                             DiscordMessageGenerator discordMessageGenerator,
-                             DiscordBotUtil discordBotUtil) {
-        this.requestStorage = requestStorage;
-        this.discordMessageGenerator = discordMessageGenerator;
-        this.discordBotUtil = discordBotUtil;
-    }
+	public ExceptionAppender(RequestStorage requestStorage,
+		DiscordMessageGenerator discordMessageGenerator,
+		DiscordBotUtil discordBotUtil) {
+		this.requestStorage = requestStorage;
+		this.discordMessageGenerator = discordMessageGenerator;
+		this.discordBotUtil = discordBotUtil;
+	}
 
-    @Before("@annotation(com.tripbook.main.global.util.discord.DiscordAlarm)")
-    public void appendExceptionToResponseBody(JoinPoint joinPoint) {
-        Object[] args = joinPoint.getArgs();
-        if (!validateHasOneArgument(args)) {
-            return;
-        }
+	@Before("@annotation(com.tripbook.main.global.util.discord.DiscordAlarm)")
+	public void appendExceptionToResponseBody(JoinPoint joinPoint) {
+		Object[] args = joinPoint.getArgs();
+		if (!validateHasOneArgument(args)) {
+			return;
+		}
 
-        if (!validateIsException(args)) {
-            return;
-        }
+		if (!validateIsException(args)) {
+			return;
+		}
 
-        String message = discordMessageGenerator
-                .generate(requestStorage.get(), (Exception) args[0]);
-        discordBotUtil.sendMessage(message);
-    }
+		String message = discordMessageGenerator
+			.generate(requestStorage.get(), (Exception)args[0]);
+		discordBotUtil.sendMessage(message);
+	}
 
-    private boolean validateIsException(Object[] args) {
-        if (!(args[0] instanceof Exception)) {
-            log.warn("[SlackAlarm] argument is not Exception");
-            return false;
-        }
+	private boolean validateIsException(Object[] args) {
+		if (!(args[0] instanceof Exception)) {
+			log.warn("[SlackAlarm] argument is not Exception");
+			return false;
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    private boolean validateHasOneArgument(Object[] args) {
-        if (args.length != 1) {
-            log.warn(String
-                    .format(DISCORD_ALARM_FORMAT, "ambiguous exceptions! require just only one Exception"));
-            return false;
-        }
+	private boolean validateHasOneArgument(Object[] args) {
+		if (args.length != 1) {
+			log.warn(String
+				.format(DISCORD_ALARM_FORMAT, "ambiguous exceptions! require just only one Exception"));
+			return false;
+		}
 
-        return true;
-    }
+		return true;
+	}
 }
