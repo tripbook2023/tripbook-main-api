d23746573c90a434e20a88c88c2637d51a31b543
lso5507 <49707719+lso5507@users.noreply.github.com>
2024-04-10 16:43:28 +0900

    fix:여행소식_검색_like추가

--- src/main/java/com/tripbook/main/article/repository/ArticleRepository.java
+++ src/main/java/com/tripbook/main/article/repository/ArticleRepository.java
@@ -46,9 +46,9 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
 		+ "        Article a1_0, \n"
 		+ "        Location l1_0 \n"
 		+ "    where\n"
-		+ "        (a1_0.title like :title escape '\\' \n"
-		+ "        or a1_0.content like :content escape '\\' \n"
-		+ "        or l1_0.name like :content escape '\\') \n"
+		+ "        (a1_0.title like concat('%',:title,'%') escape '\\' \n"
+		+ "        or a1_0.content like concat('%',:content,'%') escape '\\' \n"
+		+ "        or l1_0.name like concat('%',:content,'%') escape '\\') \n"
 		+ "        and a1_0.status=:status \n"
 		+ "        and :#{#blockIds.size()} = 0 or a1_0.member.id not in :blockIds \n"
 	)
